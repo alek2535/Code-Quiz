ceda51947ae2e74f236960f15f66a6b9db92ac46
Alek <alekvalencia2535@gmail.com>
2020-04-25 17:30:12 -0700

    if not noted before, moved btn-answer class name to li elements from buttons in html. got rid of for loop in printQuestionsAnswers while adding if else statement in btnAnswer event listener. can get questions to cycle and dynamically update html but only if you press top button.

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 //Global Variables
 let startButton = document.getElementById('start-button'); 
 let startPage = document.getElementById('start-page');
-let questionOnePage = document.getElementById('question-1');
 let timerClock = document.getElementById("timer-clock");
+let questionBlock = document.getElementById("question-block");
+let questionH1 = document.getElementById("question-h1");
+let buttonA = document.getElementById("button-a");
+let buttonB = document.getElementById("button-b");
+let buttonC = document.getElementById("button-c");
+let buttonD = document.getElementById("button-d");
+let btnAnswers = document.querySelector(".btn-answers");
 let secondsLeft = 75;
+let score = 0;
+let i = 0;
 
 //Array for Questions and Answers
 let questionsAnswers = [
     {
         question: "What kind of variable contains more than one value?",
-        choices: ["Object", "Array", "String", "Value Pair"],
+        choices: ["A) Object", "B) Array", "C) String", "D) Value Pair"],
         answer: 1
     }, 
     {
         question: "Which of these is a function in JavaScript?",
-        choices: ["var name = function() {};", "function name() {};", "() => {};", "All of the above"],
+        choices: ["A) var name = function() {};", "B) function name() {};", "C) () => {};", "D) All of the above"],
         answer: 3
     },
     {
         question: "Which of these is a library for JavaScript?",
-        choices: ["Node.js", "React.js", "jQuery", "All of the above"],
+        choices: ["A) Node.js", "B) React.js", "C) jQuery", "D) All of the above"],
         answer: 3
     }, 
     {
         question: "What keyword will not allow the variable to be reassigned or redeclared?",
-        choices: ["var", "let", "const", "None of the above"],
+        choices: ["A) var", "B) let", "C) const", "D) None of the above"],
         answer: 1
     },
     {
         question: "How do you select an element in DOM by its id?",
-        choices: ["document.getElementById()", "document.getElementByClassName()", "document.getElementByTag()", "document.getElementByName()"],
+        choices: ["A) document.getElementById()", "B) document.getElementByClassName()", "C) document.getElementByTag()", "D) document.getElementByName()"],
         answer: 0
     }  
 ];
@@ -44,17 +52,37 @@ function setTimer() {
             clearInterval(countdown);
         }
     }, 1000);
-}
+};
 
 //StartButton
 startButton.addEventListener('click', function() {
     startPage.className = 'd-none';
-    questionOnePage.className = 'container text-center mt-5';
+    questionBlock.className = 'container text-center mt-5';
+    printQuestionsAnswers();
     setTimer();
-})
+});
 
 function printQuestionsAnswers() {
-    for (let i = 0; i < questionsAnswers.length; i++) {
-        
+    questionH1.textContent = questionsAnswers[i].question;
+    buttonA.textContent = questionsAnswers[i].choices[0];
+    buttonB.textContent = questionsAnswers[i].choices[1];
+    buttonC.textContent = questionsAnswers[i].choices[2];
+    buttonD.textContent = questionsAnswers[i].choices[3];
+};
+
+
+btnAnswers.addEventListener('click', function(event) {
+    i++;
+    if (event.target.value === questionsAnswers[i].answer) {  
+        printQuestionsAnswers();
+        score++;
+        console.log(score);
+    } else {
+        printQuestionsAnswers();
     }
-}
+    // if (i < questionsAnswers.length -1) {
+    //     i++;
+    // } else {
+
+    // }
+});
